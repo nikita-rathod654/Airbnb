56ff8c07ef103463613bea50f7d8202468769d20
NikitaRathod <nr0005650@gmail.com>
2025-10-03 17:29:14 +0530

    fix index

--- backend/index.js
+++ backend/index.js
@@ -2,16 +2,16 @@ import express from "express";
 import dotenv from "dotenv";
 import connectDb from "./config/db.js";
 import authRouter from "./routes/auth.route.js";
-import cookieParser from "cookie-parser";
-import cors from "cors";
 import userRouter from "./routes/user.route.js";
 import listingRouter from "./routes/listing.route.js";
 import bookingRouter from "./routes/booking.route.js";
+import cookieParser from "cookie-parser";
+import cors from "cors";
 
 dotenv.config();
 
-const port = process.env.PORT || 6000;
 const app = express();
+const port = process.env.PORT || 6000;
 
 //  CORS Setup
 const allowedOrigins = [
@@ -32,17 +32,17 @@ app.use(cors({
   allowedHeaders: ["Content-Type", "Authorization"],
 }));
 
-// Middleware
+//  Middleware
 app.use(express.json());
 app.use(cookieParser());
 
-// Routes
+//  Routes
 app.use("/api/auth", authRouter);
 app.use("/api/user", userRouter);
 app.use("/api/listing", listingRouter);
 app.use("/api/booking", bookingRouter);
 
-// Start Server
+//  Start Server
 app.listen(port, () => {
   connectDb();
   console.log(`Server started on port ${port}`);
